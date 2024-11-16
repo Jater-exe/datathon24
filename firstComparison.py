9f9d4147aa2ec5bdd779f1c7cf92a02a46931ca4
Jater-exe <jter6868@gmail.com>
2024-11-16 13:10:49 +0100

    comparació1

---
+++ firstComparison.py
@@ -0,0 +1,13 @@
+from participant import load_participants
+
+data_path = "data/datathon_participants.json"
+participants = load_participants(data_path)
+
+#PRE: dues llistes de llenguatges
+#POST: llista dels idiomes coincidents
+def compare_languages(list)
+
+def compareUsers(user1, user2):
+    
+
+compareUsers(participants[x], participants[y])
