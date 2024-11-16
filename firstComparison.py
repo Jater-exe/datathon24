4e0942c52df6478bf359221c1d1150f3460e5ba6
Jater-exe <jter6868@gmail.com>
2024-11-16 14:07:57 +0100

    languages i availability

--- firstComparison.py
+++ firstComparison.py
@@ -8,9 +8,25 @@ participants = load_participants(data_path)
 
 #PRE: dues llistes de llenguatges
 #POST: llista dels idiomes coincidents
-def compare_languages(list)
-    
-def compare_aviailability()
+def compare_languages(list1, list2):
+    result = []
+    for language1 in list1:
+        for language2 in list2:
+            if (language1 == language2 and language1 not in result):
+                result.append(language1)
+    return result 
+
+#PRE: dos diccionaris de tipus str:bool
+#POST: una llista de strings
+def compare_availability(dict1, dict2):
+    result = []
+    keys = list(dict1.keys())
+    i = 0
+    while i < len(dict1):
+        if dict1[keys[i]] and dict2[keys[i]]:
+            result.append(keys[i])
+        i += 1
+    return result
 
 #JAUME__^^^^
  
