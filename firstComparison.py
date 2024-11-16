f45dfd34a5043e70ec374907da85b1ededa50e86
Jater-exe <jter6868@gmail.com>
2024-11-16 18:33:21 +0100

    compare years debugged

--- firstComparison.py
+++ firstComparison.py
@@ -123,25 +123,15 @@ def compare_years_study(user1_study, user2_study):
     if years_of_study_1 == years_of_study_2:
         return True
     else:
-        if years_of_study_1 == "1st Year" and (years_of_study_2 != "3rd Year" or years_of_study_2 != "4th Year"):
+        if years_of_study_1 == "1st Year" and years_of_study_2 == "2nd Year":
             return True
-        elif years_of_study_2 == "1st Year" and (years_of_study_1 != "3rd Year" or years_of_study_1 != "4th Year"):
+        elif years_of_study_1 == "2nd Year" and (years_of_study_2 == "1st Year" or years_of_study_2 == "3rd Year"):
             return True
-        elif years_of_study_1 == "2nd Year" and years_of_study_2 != "4th Year":
+        elif years_of_study_1 == "3rd Year" and (years_of_study_2 == "2nd Year" or years_of_study_2 == "4th Year"):
             return True
-        elif years_of_study_2 == "2nd Year" and years_of_study_1 != "4th Year":
+        elif years_of_study_1 == "4th Year" and years_of_study_2 == "3rd Year":
             return True
-        elif years_of_study_1 == "3rd Year" and years_of_study_2 != "1st Year":
-            return True
-        elif years_of_study_2 == "3rd Year" and years_of_study_1 != "1st Year":
-            return True
-        elif years_of_study_1 == "4th Year" and (years_of_study_2 != "1st Year" and years_of_study_2 != "2nd Year"):
-            return True
-        elif years_of_study_2 == "4th Year" and (years_of_study_1 != "1st Year" and years_of_study_1 != "2nd Year"):
-            return True
-        elif years_of_study_1 == "PhD" and years_of_study_2 == "Master's":
-            return True
-        elif years_of_study_1 == "Master's" and years_of_study_2 == "PhD":
+        elif (years_of_study_1 == "Master's" or years_of_study_1 == "PhD") and (years_of_study_2 == "Master's" or years_of_study_2 == "PhD"):
             return True
         else:
             return False
