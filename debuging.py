9c5744d30e6e06bf9ea3f55cb91a04bb29eea150
Jater-exe <jter6868@gmail.com>
2024-11-16 21:27:57 +0100

    ???

--- debuging.py
+++ debuging.py
@@ -1,6 +1,6 @@
 from participant import load_participants
 from rich import print
-import firstComparison as f1
+import first_comparasion as f1
 
 data_path = "data/datathon_participants.json"
 participants = load_participants(data_path)
@@ -33,5 +33,3 @@ for i in range(0,325):
             above0 += 1
 
 print(max, "sobre 70:", above70, "entre 50 i 70:", above50, "entre 30 i 50", above30, "inferior:", above0, UUID1, UUID2)
-
-
