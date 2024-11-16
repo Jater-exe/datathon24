18a946e41f84eac21d3e13c76f1be155fbe9d7cf
mforgas55 <marc.forgas30@gmail.com>
2024-11-16 20:51:39 +0100

    update

--- firstComparison.py
+++ firstComparison.py
@@ -12,15 +12,15 @@ participants = load_participants(data_path)
 #POST: Totes les dades coincidents
 def compareUsers(user1, user2):
     result = {}
-    coin_challenges = challenges(user1.challenges, user2.challenges)
+    coin_challenges = compare_challenge(user1.challenges, user2.challenges)
     coin_languages = compare_availability(user1.preferred_languages, user2.preferred_languages)
     coin_availability = compare_availability(user1.availability, user2.availability)
     coin_role = compare_prefered_role(user1.preferred_role, user2.preferred_role)
     coin_exp_level = compare_exp_level(user1.experience_level, user2.experience_level)
     coin_skills = compare_programming_skills(user1.programming_skills, user2.programming_skills)
     coin_studies = compare_years_study(user1.year_of_study, user2.year_of_study)
-    coin_team_size = compare_team_size(user1.preferred_team_size, user2.preferred_team_size)
-    coin_prev_hack = compare_prev_hack(user1.hackatons_done, user2.hackatons_done)
+    coin_team_size = compare_group_size(user1.preferred_team_size, user2.preferred_team_size)
+    coin_prev_hack = compare_datathons(user1.hackatons_done, user2.hackatons_done)
     coin_age = compare_age(user1.age, user2.age)
     result["challenges"] = coin_challenges
     result["languages"] = coin_languages
