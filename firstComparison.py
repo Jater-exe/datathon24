998bd78177f241b2afd70f8d38b29b6876e3c697
Jater-exe <jter6868@gmail.com>
2024-11-16 19:38:16 +0100

    compare challenge

--- firstComparison.py
+++ firstComparison.py
@@ -308,7 +308,7 @@ def compareUsers(user1, user2):
     coin_languages = compare_availability(user1.preferred_languages, user2.preferred_languages)
     coin_availability = compare_availability(user1.availability, user2.availability)
     coin_role = compare_prefered_role(user1.preferred_role, user2.preferred_role)
-    coin_exp_level = compare_exp_lvl(user1.experience_level, user2.experience_level)
+    coin_exp_level = compare_exp_level(user1.experience_level, user2.experience_level)
     coin_skills = compare_programming_skills(user1.programming_skills, user2.programming_skills)
     coin_studies = compare_years_study(user1.year_of_study, user2.year_of_study)
     coin_team_size = compare_team_size(user1.preferred_team_size, user2.preferred_team_size)
---
+++ first_comparasion/comp_challenge.py
@@ -0,0 +1,12 @@
+#PRE: Dues llistes de strings
+#POST: Una llista de strings
+
+def compare_challenge(list1, list2):
+    result = []
+    for challenge1 in list1:
+        for challenge2 in list1:
+            if challenge1 == challenge2 and challenge1 not in result:
+                result.append(challenge1)
+    return result
+
+print(compare_challenge())
