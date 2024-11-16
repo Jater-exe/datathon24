eb6bbe74913a46e855af6db6408bcb0a532fd28b
Jater-exe <jter6868@gmail.com>
2024-11-16 19:18:10 +0100

    exp_level arreglat

--- first_comparasion/comp_exp_level.py
+++ first_comparasion/comp_exp_level.py
@@ -1,30 +1,13 @@
-def compare_exp_level(interest):
-
-    grouped_pairs = []
-    ungrouped_users = []
-    used_users = set()
-
-    # Itera per el dataset per trobar parelles 
-    for i, user1 in enumerate(interest):
-        if user1['name'] in used_users:
-            continue
-        
-        # Intentar trobar coincidencia per user1
-        found_pair = False
-        for j, user2 in enumerate(interest):
-            if i == j or user2['name'] in used_users:
-                continue
-            
-            # Comprova que hi hagi alguna coincidencia als interessos
-            if set(user1['interests']).intersection(set(user2['interests'])):
-                grouped_pairs.append([user1['name'], user2['name']])
-                used_users.add(user1['name'])
-                used_users.add(user2['name'])
-                found_pair = True
-                break
-        
-        # Si no es troba parella, marcar l'usuari com a sense grup 
-        if not found_pair:
-            ungrouped_users.append(user1['name'])
-
-    return grouped_pairs, ungrouped_users
+#PRE: Dues cadenes que representen els nivells Beginner, Intermediate o Advanced
+#POST: Un booleà si coincideixen (amb tolerància)
+def compare_exp_level(exp1, exp2):
+    if exp1 == exp2:
+        return True
+    elif exp1 == "Beginner" and exp2 == "Intermediate":
+        return True 
+    elif exp1 == "Intermediate" and exp2 == "Beginner":
+        return True
+    elif exp1 == "Advanced" and exp2 == "Beginner":
+        return True
+    else:
+        return False
