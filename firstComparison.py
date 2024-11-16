02c559b1e7fefc13b5e5454515708e0242a0fa70
mforgas55 <marc.forgas30@gmail.com>
2024-11-16 13:15:51 +0100

    funcions establertes

--- firstComparison.py
+++ firstComparison.py
@@ -6,6 +6,16 @@ participants = load_participants(data_path)
 #PRE: dues llistes de llenguatges
 #POST: llista dels idiomes coincidents
 def compare_languages(list)
+    
+def compare_aviailability()
+    
+def compare_prefered_role()
+    
+def compare_programming_skills()
+    
+def compare_age()
+
+def compare_years_study()
 
 def compareUsers(user1, user2):
     
