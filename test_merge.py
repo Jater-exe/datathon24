2373911f59b68a6df2da63d1c1b724417327b9ec
Jater-exe <jter6868@gmail.com>
2024-11-17 11:02:43 +0100

    Update test_merge.py

--- test_merge.py
+++ test_merge.py
@@ -13,16 +13,14 @@ data_path_group = "data/datathon_groups.json"
 
 grups = load_groups(data_path_group)
 
-print(grups)
 # Realitzar les operacions necessàries
-grup_solo1 = grups[0] if len(grups) > 0 else None
-grup_solo2 = grups[1] if len(grups) > 1 else None
+grup_solo1 = grups[5] if len(grups) > 0 else None
+grup_solo2 = grups[6] if len(grups) > 1 else None
 
 if grup_solo1 and grup_solo2:
     merge(grup_solo1, grup_solo2, participants, grups)
 
 # Comprovar l'estat de les dades abans de guardar-les
-print("Grups abans de guardar:", grups)
-print("Participants abans de guardar:", participants)
+
 
 # Guardar les dades al fitxer JSON només si no estan buides
